1cfae08ac796ce2bdd9dfa26489a2eb739841e5f
Ramil Bikmaev <bikmaeff@gmail.com>
2013-10-30 22:11:09 +0400

    Install Spork

--- libapp/spec/dummy_spec.rb
+++ libapp/spec/dummy_spec.rb
@@ -1,8 +1,8 @@
-require 'spec_helper'
-require 'spork'
+# require 'spec_helper'
 
-describe 'dummy_spec' do 
-	it 'just passed' do
-		expect(true).to be_true
-	end
-end
+
+# describe 'dummy spec' do 
+# 	it 'just passed' do
+# 		expect(true).to be_true
+# 	end
+# end
